8f57786294ae3cbd34f2c4cd3f326bb30defef8e
Abhay Partap Singh Rana <138298735+Wicks2@users.noreply.github.com>
2023-08-10 17:24:08 +0530

    Add files via upload

---
+++ eth_avax_mod3.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("Abhay", "ABH") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function approveAndTransfer(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        _transfer(msg.sender, spender, amount);
+        return true;
+    }
+}
